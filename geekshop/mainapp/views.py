a822b0f1593457b0394090a5eef0580c6fd02887
katerina_topol <katerina_topol@mail.ru>
2021-07-03 19:43:22 +0300

    add dynamics menu

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
+from mainapp.models import ProductCategory
 
 
 def products(request):
     title = 'каталог'
     links_menu = [
-        {'href': 'products_all', 'name': 'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'},
-    ]
+        {'href': i.link, 'name': i.name}
+        for i in ProductCategory.objects.all()]
+    # links_menu = [
+    #     {'href': 'products_all', 'name': 'все'},
+    #     {'href': 'products_home', 'name': 'дом'},
+    #     {'href': 'products_office', 'name': 'офис'},
+    #     {'href': 'products_modern', 'name': 'модерн'},
+    #     {'href': 'products_classic', 'name': 'классика'},
+    # ]
     context = {
         'title': title,
         'links_menu': links_menu,
